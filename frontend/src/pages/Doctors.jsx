65081d9a62574b33192ecddbfef9d0f9c78954cd
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2026-03-18 18:01:53 +0530

    Add All doctors

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Doctors = () => {
-  return <div></div>;
+  const { speciality } = useParams();
+  const navigate = useNavigate();
+
+  const [filterDoc, setFilterDoc] = useState([]);
+  const { doctors } = useContext(AppContext);
+
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter((doc) => doc.speciality === speciality));
+    } else {
+      setFilterDoc(doctors);
+    }
+  };
+
+  useEffect(() => {
+    applyFilter();
+  }, [doctors, speciality]);
+
+  return (
+    <div>
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>
+          <div>
+            <p>General physician</p>
+            <p>Gynecologist</p>
+            <p>Dermatologist</p>
+            <p>Pediatricians</p>
+            <p>Neurologist</p>
+            <p>Gastroenterologist</p>
+          </div>
+        </div>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+          {filterDoc.map((item) => (
+            <div
+              key={item._id}
+              onClick={() => navigate(`/appointments/${item._id}`)}
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-transform duration-300"
+            >
+              <img
+                className="bg-blue-50 w-full h-48 object-cover"
+                src={item.image}
+                alt={item.name}
+                onError={(e) => {
+                  e.target.src = "/fallback.png";
+                }}
+              />
+
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-green-500">
+                  <span className="w-2 h-2 bg-green-500 rounded-full"></span>
+                  <p>{item.available ? "Available" : "Not Available"}</p>
+                </div>
+
+                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+                <p className="text-gray-600 text-sm">{item.speciality}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Doctors;
