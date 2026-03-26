08ae5f77b7a8e9b88454379818104b6142925451
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2026-03-26 15:09:02 +0530

    Auth Api

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -9,7 +9,7 @@ if (!fs.existsSync(uploadPath)) {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // ✅ required
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
