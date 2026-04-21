4aabe17d3d6bc50ad343f29942de501e146f6041
Vishalkushwaha7782 <vishalkushwaha7782@gmail.com>
2026-04-21 14:28:42 +0530

    create calculateAge function to show age in appointment page

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -3,7 +3,31 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 const AppcontextProvider = (props) => {
-  const value = {};
+  const currency = "$";
+
+  const calculateAge = (dob) => {
+    if (!dob) return "N/A";
+
+    const birthDate = new Date(dob);
+
+    if (isNaN(birthDate)) return "N/A";
+
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    return age;
+  };
+  const value = {
+    calculateAge,
+    currency,
+  };
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
